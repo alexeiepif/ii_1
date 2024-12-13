7157aa9cf055c047dfddb167dbb8d38096eab2eb
alexeiepif <aled2093746@gmail.com>
2024-12-13 17:38:44 +0300

    fix

--- program/py.py
+++ program/py.py
@@ -1,106 +1,86 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Методом полного перебора решите задачу коммивояжёра
+# Методом полного перебора решите задачу коммивояжёра h
 
 import itertools
 import json
+from dataclasses import dataclass
+from typing import Any
 
 import numpy as np
-import pandas as pd
 
 
-def read_matrix_from_excel(file_path):
-    """
-    Считывает матрицу из Excel файла.
-    """
+@dataclass
+class Nodes:
+    nodes: dict[int, str]
 
-    df = pd.read_excel(file_path, header=None)
-    city_names = df.iloc[0, 1:11].tolist()
-    df = df.iloc[1:11, 1:11]
-    matrix = df.values
-    return matrix, city_names
+    def __getitem__(self, key: int) -> str:
+        return self.nodes.get(key, "")
 
 
-def lower_triangle_to_full_matrix(lower_triangle_matrix: np.ndarray):
-    """
-    Преобразует нижнюю треугольную матрицу в полную симметричную матрицу.
-    """
+@dataclass
+class Edges:
+    edges: dict[tuple[int, int], float]
 
-    num_cities = lower_triangle_matrix.shape[0]
-    full_matrix = np.zeros((num_cities, num_cities))
-
-    for i in range(num_cities):
-        for j in range(i + 1):
-            full_matrix[i][j] = lower_triangle_matrix[i][j]
-            full_matrix[j][i] = lower_triangle_matrix[i][j]
-
-    return full_matrix
+    def __getitem__(self, key: tuple[int, int]) -> float:
+        return self.edges.get(key) or self.edges.get(key[::-1]) or np.inf
 
 
-def preprocess_matrix(matrix):
-    """
-    Преобразует знаки '-' в 0 в матрице
-    и возвращает матрицу с числовыми значениями.
-    """
-
-    matrix = np.where(matrix == "-", 0, matrix)
-    return matrix.astype(float)
-
-
-def calculate_path_length(matrix, path):
+def calculate_path_length(edges: Edges, path: list[int]) -> float:
     """
     Рассчитывает длину пути по матрице расстояний.
     """
 
-    length = 0
-    for i in range(len(path) - 1):
-        k = matrix[path[i]][path[i + 1]]
-        if np.isnan(k):
-            return np.inf
+    def find_edge_weight(source: int, target: int) -> float:
+        edge_weight = edges[source, target]
+        if edge_weight:
+            return edge_weight
+        return np.inf
 
-        length += k
-    length += matrix[path[-1]][path[0]]
+    length = sum(find_edge_weight(i, j) for i, j in zip(path, path[1:]))
+    length += find_edge_weight(path[-1], path[0])
+
     return length
 
 
-def traveling_salesman(matrix, names_cities):
+def traveling_salesman(data: list[dict[str, Any]]) -> Any:
     """
     Решает задачу коммивояжёра методом полного перебора.
     """
+    data = [x["data"] for x in data]
+    node_count = len([x for x in data if "id" in x])
+    nodes = Nodes({int(x["id"]): x["label"] for x in data[:node_count]})
+    edges = Edges(
+        {(int(x["source"]), int(x["target"])): x["weight"] for x in data[node_count:]}
+    )
 
-    num_cities = len(matrix)
-    cities = list(range(num_cities))
-    shortest_path = None
+    shortest_path = []
     min_length = np.inf
 
     permutations = [
-        ([0] + list(perm)) for perm in itertools.permutations(cities[1:])
+        ([1] + list(perm)) for perm in itertools.permutations(range(2, node_count + 1))
     ]
 
     for perm in permutations:
-        current_length = calculate_path_length(matrix, perm)
+        current_length = calculate_path_length(edges, perm)
         if current_length < min_length:
             min_length = current_length
             shortest_path = perm
 
-    shortest_path_named = [names_cities[i] for i in shortest_path]
+    shortest_path_named = []
+    for i in shortest_path:
+        name = nodes[i]
+        if name:
+            shortest_path_named.append(name)
     return shortest_path_named, shortest_path, min_length
 
 
 if __name__ == "__main__":
-    file_path = "C:/Users/MECHREVO/Desktop/пары/пары_5_сем/ИИвПС/graph2.xlsx"
-    lower_triangle_matrix, names_cities = read_matrix_from_excel(file_path)
-    processed_matrix = preprocess_matrix(lower_triangle_matrix)
-    full_matrix = lower_triangle_to_full_matrix(processed_matrix)
-    print(full_matrix)
+    file_path = "data/graph2.xlsx"
+    with open("json/elem_full.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
 
-    shortest_path_named, shorted_path, min_length = traveling_salesman(
-        full_matrix, names_cities
-    )
-
-    with open("kommivoyager_path.json", "w") as f:
-        json.dump(shorted_path, f)
-
-    print(f"Самый короткий путь: {shortest_path_named},")
+    shortest_path_named, shortest_path, min_length = traveling_salesman(data)
+    print(f"Самый короткий путь: {shortest_path_named}")
     print(f"Длина самого короткого пути: {min_length}")
